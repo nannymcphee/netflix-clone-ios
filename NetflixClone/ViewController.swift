d587f26559250d0013b9d6396b22ceb6fc40bb14
Duy Nguyen <duynn@neoconsulting.com.vn>
2022-03-31 14:00:03 +0700

    Initial Commit

---
+++ NetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Duy Nguyen on 31/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
